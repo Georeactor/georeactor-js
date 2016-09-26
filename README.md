db3d19a82510473b131ce3373fa30aa1e28326d2
Nick Doiron <ndoiron@mapmeld.com>
2016-09-25 23:35:09 -0400

    data transform options for your fields

--- README.md
+++ README.md
@@ -1,12 +1,71 @@
 # GeoReactor JS
 
-Georeactor is an experimental interface for creating interactive, mobile-responsive maps.
+GeoReactor is an experimental library for creating interactive, mobile-responsive maps.
+
+* Place your data on top of Google Maps or OpenStreetMap
+* Developer-friendly presets, user-friendly UI for different datasets
+* See it on desktop, mobile, and embedded maps
+* Update your data and refresh the map
 
 ## Examples
 
 * <a href="http://georeactor.github.io/georeactor-js/index.html">Google Maps API</a>
 
 * <a href="http://georeactor.github.io/georeactor-js/leaflet.html">Leaflet + OSM</a>
+
+#### A basic map
+
+Include ReactJS, the common GeoReactor Client library, and the Google Maps (gmaps) extension.
+
+Send an array of data URLs to the library to download and display that data.
+
+```html
+<div id='map'></div>
+...
+<script src='react.min.js'></script>
+<script src='georeactor-client.js'></script>
+<script src='georeactor-gmaps.js'></script>
+<script>
+  georeactor({
+    div: 'map',
+    data: ['data/link.geojson']
+  })
+</script>
+```
+
+#### A React table UI
+
+Show information about map features using a ReactJS table. Control your data's appearance in the
+display.
+
+```html
+<div id='map'></div>
+...
+<script src='react.min.js'></script>
+<script src='georeactor-client.js'></script>
+<script src='georeactor-gmaps.js'></script>
+<script type='text/jsx' src='georeactor-detail.js'></script>
+<script>
+  georeactor({
+    div: 'map',
+    data: ['data/link.geojson'],
+
+    /* data transform options */
+    attributes: {
+      _order: ['Title', 'AUTHOR', 'Description'],
+      AUTHOR: {
+        label: 'Author',
+        value: 'Created by #{AUTHOR}'
+      },
+      Description: {
+        value: function (key, val) {
+          return val.substring(0, 100);
+        }
+      }
+    }
+  })
+</script>
+```
 
 ## License
 
--- scripts/georeactor-detail.js
+++ scripts/georeactor-detail.js
@@ -2,6 +2,10 @@
   var valuesForField = {};
   var calledBefore = false;
 
+  var RegExpEscape = function(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+  };
+
   GEOREACTOR.initReact = function(){
     // only run init once
     if (calledBefore) {
@@ -16,20 +20,27 @@
     var MapLabel = React.createClass({
       render: function() {
         var adjustedLabel = this.props.label;
-        var adjustedValue;
-        if (this.props.value) {
-          adjustedValue = this.props.value;
+        var adjustedValue = this.props.value || '';
+
+        if (GEOREACTOR.options && GEOREACTOR.options.attributes && GEOREACTOR.options.attributes[adjustedLabel]) {
+          var adjustments = GEOREACTOR.options.attributes[adjustedLabel];
+          if (typeof adjustments.label === 'function') {
+            adjustedLabel = adjustments.label(this.props.label, this.props.value);
+          } else if (typeof adjustments.label === 'string') {
+            adjustedLabel = adjustments.label;
+          }
+          if (typeof adjustments.value === 'function') {
+            adjustedValue = adjustments.value(this.props.label, this.props.value);
+          } else if (typeof adjustments.value === 'string') {
+            var labelrg = new RegExp('#\\{' + RegExpEscape(this.props.label) + '\\}', 'g');
+            adjustedValue = adjustments.value.replace(labelrg, adjustedValue);
+          }
         }
-        /*
-        else if (GEOREACTOR.library === 'gmaps') {
-          adjustedValue = selectFeature.getProperty(this.props.label);
-        } else {
-          adjustedValue = selectFeature.properties[this.props.label];
-        }
-        */
+
         if (typeof adjustedValue === 'object') {
           adjustedValue = JSON.stringify(adjustedValue);
         }
+
         return (<div className="field">
           <div className="head">{adjustedLabel}</div>
           <div className="fieldVal">{adjustedValue}</div>
@@ -67,17 +78,24 @@
               }
             });
           }
-          var titleLabel = <h1></h1>;
-          if (this.state.title) {
-            var fieldGuide = this.state.codeForField[this.state.title];
-            var regularValue;
-            if (GEOREACTOR.library === 'gmaps') {
-              regularValue = this.state.selectFeature.getProperty(this.state.title);
-            } else {
-              regularValue = this.state.selectFeature.properties[this.state.title];
-            }
-            titleLabel = <h1>{fieldGuide.state.metalabel.replace('{' + this.state.title + '}', regularValue)}</h1>;
+
+          // optional ordering of properties
+          if (GEOREACTOR.options.attributes && GEOREACTOR.options.attributes._order) {
+            properties.sort(function(a,b) {
+              var aOrder = GEOREACTOR.options.attributes._order.indexOf(a.label);
+              var bOrder = GEOREACTOR.options.attributes._order.indexOf(b.label);
+              if (aOrder !== bOrder) {
+                if (aOrder === -1) {
+                  return 1;
+                }
+                if (bOrder === -1) {
+                  return -1;
+                }
+              }
+              return aOrder - bOrder;
+            });
           }
+
           return (<div className='table'>
             {properties.map(function(chr, i) {
               return <MapLabel label={chr.label} value={chr.value} visible={this.state.viewFieldIndex === i}/>;
